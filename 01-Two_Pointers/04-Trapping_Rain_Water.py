5741c449b453fd925edc23594aae45e6ae44a6ae
uygarpolat <uygarpolat@gmail.com>
2025-05-20 17:10:24 +0300

    04-Trapping_Rain_Water.py added

--- 01-Two_Pointers/04-Trapping_Rain_Water.py
+++ 01-Two_Pointers/04-Trapping_Rain_Water.py
@@ -1,5 +1,6 @@
 """
 Trapping Rain Water
+
 https://neetcode.io/problems/trapping-rain-water
 
 You are given an array non-negative integers height which represent an elevation map. Each value height[i] represents the height of a bar, which has a width of 1.
@@ -20,14 +21,33 @@ Constraints:
 from typing import List
 
 class Solution:
-    def trap(self, height: List[int]) -> int:
-        return 0
+	def trap(self, height: List[int]) -> int:
+
+		length = len(height)
+		prefix = [0] * length
+		suffix = [0] * length
+		prefix[0] = 0
+		suffix[length-1] = 0
+		result = 0
+        
+		for i in range(1, length):
+			prefix[i] = max(prefix[i-1], height[i-1])
+
+		for i in range(length-2,-1,-1):
+			suffix[i] = max(suffix[i+1], height[i+1])
+
+		for i in range(length):
+			result += max(0, min(prefix[i],suffix[i]) - height[i])
+             
+		return result
     
 def main():
-    solution = Solution()
-    height = [0,2,0,3,1,0,1,3,2,1]
-    result = solution.trap(height)
-    assert(height == 9)
+	solution = Solution()
+	height = [0,2,0,3,1,0,1,3,2,1]
+	result = solution.trap(height)
+	assert(result == 9)
+
+	print("✅ All tests passed!")
     
 if __name__ == "__main__":
     main()
