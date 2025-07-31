923c44215cfc25a7837f69f96bf9830d3a56b220
uygarpolat <uygarpolat@gmail.com>
2025-07-31 17:59:21 +0300

    06-Partition_Labels.py added

--- 14-Greedy/06-Partition_Labels.py
+++ 14-Greedy/06-Partition_Labels.py
@@ -1,6 +1,8 @@
 """
 Partition Labels
-Solved 
+
+https://neetcode.io/problems/partition-labels?list=neetcode150
+
 You are given a string s consisting of lowercase english letters.
 
 We want to split the string into as many substrings as possible, while ensuring that each letter appears in at most one substring.
@@ -22,21 +24,6 @@ Output: [6]
 Constraints:
 
 1 <= s.length <= 100
-
-Recommended Time & Space Complexity
-You should aim for a solution with O(n) time and O(m) space, where n is the length of the string s and m is the number of unique characters in the string s.
-
-
-Hint 1
-A character has a first and last index in the given string. Can you think of a greedy approach to solve this problem? Maybe you should try iterating over one of these two indices.
-
-
-Hint 2
-We store the last index of each character in a hash map or an array. As we iterate through the string, treating each index as a potential start of a partition, we track the end of the partition using the maximum last index of the characters seen so far in the current partition. Additionally, we maintain the size of the current partition using a variable, say size.
-
-
-Hint 3
-We update the end of the current partition based on the maximum last index of the characters, extending the partition as needed. When the current index reaches the partition’s end, we finalize the partition, append its size to the output list, reset the size to 0, and continue the same process for the remaining string.
 """
 from typing import List
 from collections import defaultdict
